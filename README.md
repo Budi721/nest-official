aa5dd48b5ac56b0c6de194ff7c8dd91b1d57f4bf
Golek PC <golek_pc@golektruck.dev>
2022-09-29 09:02:11 +0700

    close: section 1 course

---
+++ README.md
@@ -0,0 +1,5 @@
+### Untuk Validasi di Nest Js
+
+install package `class-validator class-transformer`
+
+utility untuk remove redundant code duplication `@nestjs/mapped-types`
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      brand: 'Torabika',
+      flavors: ['mocha'],
+      name: 'Kopi Sachet',
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    return this.coffees.find((coffee) => coffee.id === +id);
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // update existing coffee
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((coffee) => coffee.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
